7f0ff6aeeb52f11e58c68e5db5a391c4dfec6566
Chris Ward <chriswardchrisward@gmail.com>
2017-02-06 15:15:11 -0800

    test

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react'; //loads everything in react library into React variable
+import { render } from 'react-dom'; //We do this to render HTML if we were writing an app we would use something else, curly brackets are here to just import one method from 'react-dom'
+
+
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main'));
