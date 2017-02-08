4ce97cd179b9de222cfb35019c7d86ae81ec8496
Chris Ward <chriswardchrisward@gmail.com>
2017-02-08 13:30:43 -0800

    v.6 creating application layout with components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'; //loads everything in react library into React variable
 import { render } from 'react-dom'; //We do this to render HTML if we were writing an app we would use something else, curly brackets are here to just import one method from 'react-dom'
+import './css/style.css'; //This takes everything in style.css and webpack compiles it all
 
 
-import StorePicker from './components/StorePicker';
+import StorePicker from './components/StorePicker'; 
+import App from './components/App'
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
