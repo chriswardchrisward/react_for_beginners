fb2e3590526548cd2934408accb2846cc0c6e776
Chris Ward <chriswardchrisward@gmail.com>
2017-02-20 10:15:40 -0800

    v.9 Routing with React Router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,26 @@
 import React from 'react'; //loads everything in react library into React variable
 import { render } from 'react-dom'; //We do this to render HTML if we were writing an app we would use something else, curly brackets are here to just import one method from 'react-dom'
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css'; //This takes everything in style.css and webpack compiles it all
 
 
-// import StorePicker from './components/StorePicker'; 
-import App from './components/App'
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App/>, document.querySelector('#main'));
+const Root = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={StorePicker} />
+				<Match exactly pattern="/store/:storeId" component={App} />
+				<Miss component={NotFound} />			
+			</div>
+		</BrowserRouter>
+	)
+}
+
+render(<Root/>, document.querySelector('#main'));
+
+//routing
