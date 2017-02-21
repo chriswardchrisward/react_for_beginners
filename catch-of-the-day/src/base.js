29b74644c9254d8779fca3e83b9c41061d4f5625
Chris Ward <chriswardchrisward@gmail.com>
2017-02-20 23:34:52 -0800

    Persisting our State with Firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyB0-NXybyGJT6Fgdk7hHr3aVU9CH4w9VJU",
+    authDomain: "catch-of-the-day-chris-ward.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-chris-ward.firebaseio.com",
+});
+
+export default base;
